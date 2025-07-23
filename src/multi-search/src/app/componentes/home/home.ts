702444ba1320f07baeb3244486292aa3b3dfb21d
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2025-07-23 14:39:13 -0300

    feat: implementado lista de produtos

--- src/multi-search/src/app/componentes/home/home.ts
+++ src/multi-search/src/app/componentes/home/home.ts
@@ -4,11 +4,12 @@ import { ShowSearch } from '../show-search/show-search';
 import { ShowPurchaseOrder } from '../show-purchase-order/show-purchase-order';
 import { ShowSaleOrder } from '../show-sale-order/show-sale-order';
 import { ShowEquiptment } from '../show-equiptment/show-equiptment';
+import { ShowMaterial } from '../show-material/show-material';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [ShowSearch, ShowPurchaseOrder, ShowSaleOrder, ShowEquiptment],
+  imports: [ShowSearch, ShowPurchaseOrder, ShowSaleOrder, ShowEquiptment, ShowMaterial],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
--- src/multi-search/src/app/componentes/show-material/show-material.ts
+++ src/multi-search/src/app/componentes/show-material/show-material.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PurchaseOrdersService } from '../../services/purchaseorders.service';
+import { MaterialService } from '../../services/material.service';
+import { Material } from '../../models/material.interface';
 
 @Component({
   selector: 'app-show-material',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './show-material.html',
   styleUrl: './show-material.css'
 })
-export class ShowMaterial {
+export class ShowMaterial  implements OnInit {
+  loading = false;
+  error: string | null = null;
+  materials: Material[] = [];
+  
+  constructor(private materialService: MaterialService, private cdr: ChangeDetectorRef) {}
 
+  ngOnInit(): void {
+    this.loadMaterial();
+  }
+
+  loadMaterial() {
+    this.loading = true;
+    this.error = null;
+  
+    this.materialService.getEquipments().subscribe({
+      next: (data) => {
+        this.materials = data;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.error = 'Erro ao carregar equipamentos: ' + err.message;
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
